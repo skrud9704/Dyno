2c93da2489e70bd4efa812220f198eb90b9d1226
jeewoo9719 <34128041+jeewoo9719@users.noreply.github.com>
2020-07-18 16:55:23 +0900

    Room 사용하여 Sqlite에 사용자 정보 저장하기
    user 이름이랑 디바이스 아이디 저장

--- Dyno/app/src/main/java/com/example/dyno/SignUp.kt
+++ Dyno/app/src/main/java/com/example/dyno/SignUp.kt
@@ -6,20 +6,33 @@ import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.widget.EditText
+import android.widget.TextView
+import com.example.dyno.LocalDB.User
+import com.example.dyno.LocalDB.UserDB
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUp : AppCompatActivity() {
+    private var userDB:UserDB?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        var et=findViewById(R.id.editTextTextPersonName) as EditText
+        val newUser = User()
+
+        var et= findViewById<EditText>(R.id.editTextTextPersonName) //사용자 이름 받아오기 위한 거
+        var tv= findViewById<TextView>(R.id.userD)// 사용자 단말기 아이디 보여주는거
+
         var userDevice=Settings.Secure.getString(this.contentResolver,Settings.Secure.ANDROID_ID)
-        userD.setText(userDevice)
+        tv.setText(userDevice)
+        newUser.dId=userDevice
+
 
         Log.d("device info",userDevice)
         button.setOnClickListener{
-            var userName : String =et.text.toString()
+            userDB=UserDB.getInstance(this)
+            newUser.uName =et.text.toString()
+            userDB?.userDao()?.insertUser(newUser)
+           
 
         }
 
