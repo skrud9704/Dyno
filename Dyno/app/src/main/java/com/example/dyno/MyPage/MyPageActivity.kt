e1f8fab8ed22e26b8c664ca513172306147bdf4c
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-09-17 20:14:35 +0900

    MyPage UserName Set
    마이페이지 사용자 이름 SharedPreferences에서 가져와서 set.

--- Dyno/app/src/main/java/com/example/dyno/MyPage/MyPageActivity.kt
+++ Dyno/app/src/main/java/com/example/dyno/MyPage/MyPageActivity.kt
@@ -1,5 +1,7 @@
 package com.example.dyno.MyPage
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +10,9 @@ import com.example.dyno.R
 import kotlinx.android.synthetic.main.activity_my_page.*
 
 class MyPageActivity : AppCompatActivity() {
+    var SHARED_PREF = "SaveLogin"
+    var SHARED_PREF_NAME = "name"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
@@ -15,6 +20,11 @@ class MyPageActivity : AppCompatActivity() {
         val myPageAdapter = MyPageAdapter(supportFragmentManager,1)
         viewPager.adapter = myPageAdapter
         tab.setupWithViewPager(viewPager)
+
+        val pref : SharedPreferences = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
+        val prefName = pref.getString(SHARED_PREF_NAME,"null")
+        if(prefName!="null")
+            myPageName.text = prefName
 
     }
 }
