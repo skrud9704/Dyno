cd9b5e1f18d76aca89418e763b80f40285c35681
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-09-24 19:34:30 +0900

    Dyno 로고, 디자인 추가.
    splash 완성
    main 화면 재구성

--- Dyno/app/src/main/java/com/example/dyno/MainActivity.kt
+++ Dyno/app/src/main/java/com/example/dyno/MainActivity.kt
@@ -19,10 +19,6 @@ class MainActivity : AppCompatActivity() {
         val adapter=MainAdapter(whatIEatList)
         mPager.adapter=adapter
 
-        val detailInfo= findViewById<Button>(R.id.viewAll)
-        detailInfo.setOnClickListener{
-            //val intent =Intent(this,)
-        }
         val registerDiseasePage=findViewById<Button>(R.id.registerM)
         registerDiseasePage.setOnClickListener{
             val nextIntent=Intent(this, Camera::class.java)
