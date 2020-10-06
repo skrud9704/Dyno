2b40341fa7115cd4e3af4bc83d102ee8460c862b
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-10-06 15:59:04 +0900

    Expandable 일단 보류.
    다른 기능 먼저 완료 후 시간 남으면 계속 진행하는걸로.

--- Dyno/app/src/main/java/com/example/dyno/Detail/Adapters/DetailSAdapter.kt
+++ Dyno/app/src/main/java/com/example/dyno/Detail/Adapters/DetailSAdapter.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dyno.Detail.Adapters.DetailSAdapter.ViewHolder
@@ -27,8 +28,6 @@ class DetailSAdapter(context: Context, ingredients : ArrayList<String>, infos : 
     // Expandable한 UI를 위해 필요한 변수
     // 1. Item의 클릭 상태를 저장할 배열
     var selectedItems : SparseBooleanArray = SparseBooleanArray()
-    // 2. 직전에 클릭했던 item의 position
-    var prePosition = -1
 
     // 초기화. (init을 지우고 위에 맴버변수에 바로 할당해도 됨.)
     init {
@@ -52,22 +51,48 @@ class DetailSAdapter(context: Context, ingredients : ArrayList<String>, infos : 
 
     // Data 와 커스텀 ViewHolder를 Bind하는 부분.
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // 기능성원재료 이름 TextView
         holder.ingredient_tv.text = ingredientModels[position]
+        // 기능성내용 TextView
         holder.info_tv.text = infoModels[position]
-        holder.expand_btn.setOnClickListener {
+
+        // 확장 버튼 클릭 시 = 접힌 아이템을 펼칠 때
+        /*holder.expand_btn.setOnClickListener {
+            // 버튼 바꿔치기
+            holder.expand_btn.visibility = View.GONE
+            holder.fold_btn.visibility = View.VISIBLE
+
+            /*
+            // 펼쳐진 아이템 클릭시
             if(selectedItems.get(position)){
                 selectedItems.delete(position)
-            }else{
+            }
+            // 접힌 아이템 클릭시
+            else{
+                // 직전의 클릭했던 item의 클릭상태를 지움
                 selectedItems.delete(prePosition)
+                // 클릭한 item의 position 저장
                 selectedItems.put(position,true)
             }
-
+            // 해당 포지션의 변화를 알림
             if(prePosition!=-1)
                 notifyItemChanged(prePosition)
             notifyItemChanged(position)
+            // 클릭된 position 저장
+            prePosition = position*/
 
-            prePosition = position
+            changeVisibility(true,holder)
         }
+
+        // 접기 버튼 클릭 시 = 확장된 아이템을 접을 때
+        holder.fold_btn.setOnClickListener {
+            // 버튼 바꿔치기
+            holder.fold_btn.visibility = View.GONE
+            holder.expand_btn.visibility = View.VISIBLE
+
+
+            changeVisibility(false,holder)
+        }*/
     }
 
     override fun getItemId(position: Int): Long {
@@ -78,8 +103,9 @@ class DetailSAdapter(context: Context, ingredients : ArrayList<String>, infos : 
         return position
     }
 
+    // Expandable한 Item을 위해 기능성내용의 Visibility를 조정하는 함수.
     private fun changeVisibility(isExpanded : Boolean, holder: ViewHolder){
-        val dpValue=150
+        val dpValue=holder.info_layout.layoutParams.height
         val d = context.resources.displayMetrics.density
         val height = (dpValue*d).toInt()
         var va : ValueAnimator = ValueAnimator()
@@ -91,27 +117,34 @@ class DetailSAdapter(context: Context, ingredients : ArrayList<String>, infos : 
 
         va.duration=600
         va.addUpdateListener {
-
+            val value = (it.animatedValue) as Int
+            holder.info_layout.layoutParams.height = value
+            holder.info_layout.requestLayout()
+            if(isExpanded){
+                holder.info_layout.visibility = View.VISIBLE
+            }
+            else {
+                holder.info_layout.visibility = View.GONE
+            }
         }
-
-        //va.start()
+        va.start()
     }
 
 
     // 커스텀 ViewHolder, 리사이클러뷰의 각각의 아이템마다 데이터를 넣을 View 를 미리 선언하고 가져옴, 클릭리스너 implement
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var ingredient_tv : TextView
         var info_tv : TextView
         var expand_btn : ImageView
+        var fold_btn : ImageView
+        var info_layout : LinearLayout
 
         init{
             ingredient_tv = itemView.ingredient_name
             info_tv = itemView.ingredient_info
             expand_btn = itemView.expand_btn
-        }
-
-        override fun onClick(v: View?) {
-            TODO("Not yet implemented")
+            fold_btn = itemView.fold_btn
+            info_layout = itemView.info_layout
         }
 
     }
