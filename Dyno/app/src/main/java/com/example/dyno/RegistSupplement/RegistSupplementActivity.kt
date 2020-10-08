858df36ba1ff02200be4b0ade8ca39717356ffa2
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-10-08 18:29:45 +0900

    RegistSupplementAdapter 리스트뷰 -> 리싸이클러뷰로 변경

--- Dyno/app/src/main/java/com/example/dyno/RegistSupplement/RegistSupplementActivity.kt
+++ Dyno/app/src/main/java/com/example/dyno/RegistSupplement/RegistSupplementActivity.kt
@@ -1,10 +1,9 @@
 package com.example.dyno.RegistSupplement
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.webkit.JavascriptInterface
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dyno.R
 import com.example.dyno.ServerAPI.RetrofitService
 import com.example.dyno.ServerAPI.RetrofitClient
@@ -13,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_regist_supplement.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
 
 // process
 // 0. 이름으로 검색.
@@ -21,40 +21,52 @@ import retrofit2.Response
 // 3. 1.에서 없다면 웹에서 크롤링 후 RDS 저장, 어플에서 등록.
 // 일단, RDS 절차 건너뛰고 3부터.
 
-var siteSource : String =""
-
 class RegistSupplementActivity : AppCompatActivity() {
 
-    val TAG = this::class.java.simpleName
+    private val TAG = this::class.java.simpleName
+    private lateinit var retrofit : Retrofit
+    private lateinit var supplementService : RetrofitService
+    var dataList : ArrayList<SupplementVO> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regist_supplement)
 
-        val retrofit = RetrofitClient.getInstance()
-        val supplementService = retrofit.create(RetrofitService::class.java)
+        // 어댑터 달기
+        search_list.adapter = SupplementAdapter(this,dataList)
+        search_list.layoutManager= LinearLayoutManager(this)
 
+        // 서버 연결
+        initRetrofit()
+
+        // 버튼 클릭 시
         btn_search.setOnClickListener {
-
-
-
-            /*supplementService.requestSupplement(input_search.text.toString()).enqueue(object : Callback<SupplementVO>{
-                override fun onFailure(call: Call<SupplementVO>, t: Throwable) {
-                    Log.d(TAG,"실패 : $t")
-                }
-
-                override fun onResponse(call: Call<SupplementVO>, response: Response<SupplementVO>) {
-                    Log.d(TAG,"성공^^")
-                    Log.d(TAG,"이름 : ${response.body()!!.m_name}")
-                    Log.d(TAG,"회사 : ${response.body()!!.m_company}")
-                    Log.d(TAG,"날짜(없어야 정상) : ${response.body()!!.m_date}")
-                    Log.d(TAG,"재료명 리스트 : ${response.body()!!.m_ingredients}")
-                    Log.d(TAG,"재료명 정보 : ${response.body()!!.m_ingredients_info}")
-                }
-
-            })*/
+            connect(supplementService)
         }
 
+    }
+
+    private fun initRetrofit(){
+        retrofit = RetrofitClient.getInstance()
+        supplementService = retrofit.create(RetrofitService::class.java)
+    }
+
+    private fun connect(service : RetrofitService){
+        service.requestSupplementSimple(input_search.text.toString()).enqueue(object : Callback<ArrayList<SupplementVO>>{
+            override fun onFailure(call: Call<ArrayList<SupplementVO>>, t: Throwable) {
+                Log.d(TAG,"실패")
+            }
+
+            override fun onResponse(
+                call: Call<ArrayList<SupplementVO>>,
+                response: Response<ArrayList<SupplementVO>>
+            ) {
+                Log.d(TAG,"성공^^")
+                // 데이터 저장
+                dataList = response.body()!!
+                search_list.adapter!!.notifyDataSetChanged()
+            }
+        })
     }
 
 
