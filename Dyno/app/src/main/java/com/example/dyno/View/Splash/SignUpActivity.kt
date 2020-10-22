68603085b3d0f09b0bbbf56fff9769063bbd0605
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-10-22 13:17:53 +0900

    SignUpActivity 수정
    동의 체크박스 눌러야 넘어갈 수 있도록 변경

--- Dyno/app/src/main/java/com/example/dyno/View/Splash/SignUpActivity.kt
+++ Dyno/app/src/main/java/com/example/dyno/View/Splash/SignUpActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
 import com.example.dyno.VO.UserVO
 import com.example.dyno.View.Main.MainActivity
 import com.example.dyno.R
@@ -20,11 +21,13 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var mLocalDatabase : FirebaseDatabase
     private lateinit var mRef : DatabaseReference
 
-    var SHARED_PREF = "SaveLogin"
-    var SHARED_PREF_NAME = "name"
-    var SHARED_PREF_DID = "did"
+    private val SHARED_PREF = "SaveLogin"
+    private val SHARED_PREF_NAME = "name"
+    private val SHARED_PREF_DID = "did"
 
-    var TABLE_NAME = "USERS"
+    private val TABLE_NAME = "USERS"
+
+    private var isConsent : Boolean = false
 
     @SuppressLint("HardwareIds")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,30 +44,39 @@ class SignUpActivity : AppCompatActivity() {
         // 로그 확인.
         Log.d("device info",deviceId)
 
+        consent.setOnCheckedChangeListener { buttonView, isChecked ->
+            isConsent = isChecked
+        }
+
         // 가입 버튼 클릭 시
         button.setOnClickListener{
-            // 1. 사용자 명 저장.
-            userName = userN.text.toString()
+            if(isConsent){
+                // 1. 사용자 명 저장.
+                userName = userN.text.toString()
 
-            // 2. 사용자 객체 생성
-            val newUser = UserVO(deviceId, userName)
+                // 2. 사용자 객체 생성
+                val newUser = UserVO(deviceId, userName)
 
-            // 3. 파이어베이스 데이터 삽입
-            // 노드 형태 : "USERS"(테이블 명) 아래 "Device ID"(한 데이터의 Key) 아래 "User Name"(그 데이터 아래 key 아닌 값)
-            insertUserData(newUser)
+                // 3. 파이어베이스 데이터 삽입
+                // 노드 형태 : "USERS"(테이블 명) 아래 "Device ID"(한 데이터의 Key) 아래 "User Name"(그 데이터 아래 key 아닌 값)
+                insertUserData(newUser)
 
-            // 4. 자동 로그인을 위한 sharedPreference
-            // 나중에 앱 재시작 시 -> 두 값(SHARED_PREF_NAME, SHARED_PREF_DID)이 ""이 아니면 현재 액티비티(SignUpActivity.kt)는 건너뜀.
-            val pref : SharedPreferences = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
-            val editor = pref.edit()
+                // 4. 자동 로그인을 위한 sharedPreference
+                // 나중에 앱 재시작 시 -> 두 값(SHARED_PREF_NAME, SHARED_PREF_DID)이 ""이 아니면 현재 액티비티(SignUpActivity.kt)는 건너뜀.
+                val pref : SharedPreferences = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
+                val editor = pref.edit()
 
-            editor.putString(SHARED_PREF_NAME,newUser.userName)
-            editor.putString(SHARED_PREF_DID,newUser.deviceId)
-            editor.apply()
+                editor.putString(SHARED_PREF_NAME,newUser.userName)
+                editor.putString(SHARED_PREF_DID,newUser.deviceId)
+                editor.apply()
 
-            // 5. 메인 액티비티로 이동
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+                // 5. 메인 액티비티로 이동
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }else{
+                Toast.makeText(this,"약관에 동의하셔야 합니다.", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
 
