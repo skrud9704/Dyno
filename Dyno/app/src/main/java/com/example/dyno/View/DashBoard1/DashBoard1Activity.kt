0f76766404b2c4127e69965835d861608b926eb4
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-11-13 16:33:43 +0900

    DashBoard1Activity, DashBoard1DetailActivity 추가

---
+++ Dyno/app/src/main/java/com/example/dyno/View/DashBoard1/DashBoard1Activity.kt
@@ -0,0 +1,12 @@
+package com.example.dyno.View.DashBoard1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dyno.R
+
+class DashBoard1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board1)
+    }
+}
