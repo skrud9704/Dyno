2bb42d81ef517aa5db59cf9801a976a637ab44d8
jeewoo9719 <34128041+jeewoo9719@users.noreply.github.com>
2020-11-13 22:03:33 +0900

    대시보드 디테일에 사진 삽입

--- Dyno/app/src/main/java/com/example/dyno/View/DashBoard1/DashBoard1DetailActivity.kt
+++ Dyno/app/src/main/java/com/example/dyno/View/DashBoard1/DashBoard1DetailActivity.kt
@@ -49,7 +49,7 @@ class DashBoard1DetailActivity : AppCompatActivity(){
         warn_detail_d_list.adapter = adapter
 
         warn_detail_s_image.setImageBitmap(
-            decodeSampledBitmapFromResource(resources, R.drawable.ic_sample_mt, 100, 100)
+            decodeSampledBitmapFromResource(resources, getImgsrc(key), 100, 100)
         )
     }
 
@@ -109,5 +109,40 @@ class DashBoard1DetailActivity : AppCompatActivity(){
 
     interface OnItemDeleteClickListener{
         fun onClick(id : String)
+    }
+
+    fun getImgsrc(name:String):Int{
+        var resId:Int=-1
+        when(name){
+            "인삼" -> resId=R.drawable.ic_sample_ginseng
+            "프로바이오틱스"->resId=R.drawable.ic_sample_probiotics
+            "알로에"->resId=R.drawable.ic_sample_aloe
+            "오메가-3 지방산"->resId=R.drawable.ic_sample_omega3
+            "밀크씨슬"->resId=R.drawable.ic_sample_mt
+            "감마리놀렌산"->resId=R.drawable.ic_sample_gong
+            "당귀"->resId=R.drawable.ic_sample_danggui
+            "마테"->resId=R.drawable.ic_sample_mate
+            "돌외잎"->resId=R.drawable.ic_sample_dolwue
+            "대두"->resId=R.drawable.ic_sample_big_bean
+            "카르니틴"->resId=R.drawable.ic_sample_carnitine
+            "녹차"->resId=R.drawable.ic_sample_greentea
+            "키토산"->resId=R.drawable.ic_sample_chitosan
+            "스피루리나"->resId=R.drawable.ic_sample_spirulina
+            "석류"->resId=R.drawable.ic_sample_seokryu
+            "공액리놀레산"->resId=R.drawable.ic_sample_cla
+            "와일드망고"->resId=R.drawable.ic_sample_mango
+            "클로렐라"->resId=R.drawable.ic_sample_chlorella
+            "글루코사민"->resId=R.drawable.ic_sample_glucosamin
+            "가시오가피"->resId=R.drawable.ic_sample_gasiogapi
+            "코엔자임Q10"->resId=R.drawable.ic_sample_coenzyme
+            "은행"->resId=R.drawable.ic_sample_eunhaeng
+            "쏘팔메토추출물"->resId=R.drawable.ic_sample_ssopalmeto
+            "크랜베리"->resId=R.drawable.ic_sample_cranberry
+            "포스파티딜세린"->resId=R.drawable.ic_sample_postafatidylserine
+            "감초추출물"->resId=R.drawable.ic_sample_gamcho
+            "커뮤닌"->resId=R.drawable.ic_sample_ulgeum
+            else->resId=R.drawable.ic_sample_mt
+        }
+        return resId
     }
 }
